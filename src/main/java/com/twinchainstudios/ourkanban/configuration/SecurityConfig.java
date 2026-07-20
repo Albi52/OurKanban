cb1003e630b74fbe5c5e76f9862504c9725e7bb0
Miguel Lazaro Gil <lazaro.gil.miguel@gmail.com>
2026-07-20 14:30:17 +0200

    Update SecurityConfig.java

--- src/main/java/com/twinchainstudios/ourkanban/configuration/SecurityConfig.java
+++ src/main/java/com/twinchainstudios/ourkanban/configuration/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.twinchainstudios.ourkanban.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
