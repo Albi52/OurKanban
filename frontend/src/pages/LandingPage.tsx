e499ba95b27f5a51409347989716fbf584864a88
Miguel Lazaro Gil <lazaro.gil.miguel@gmail.com>
2026-08-07 01:55:19 +0200

    Update LandingPage.tsx

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { Button } from '@components/shared/ui/button'
 import { ArrowUpRight } from 'lucide-react'
 import { useAuth } from '@context/AuthContext'
